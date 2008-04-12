333c99fbff54d596288edc3f427f8fc521fc281a
ashley <ashley@ba90ee93-4649-0410-b91f-c47b0897d458>
2008-04-12 08:59:48 +0000

    Committing Skizzerz's GlobalCssJs extension. I've tweaked the comments, no other changes.

---
+++ trunk/mediawiki/extensions/GlobalCssJs/GlobalCssJs.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Internationalisation file for the Global CSS/JS extension
+ * @author Ryan Schmidt  <skizzerz@gmail.com>
+ * @copyright © 2008 Ryan Schmidt
+ */
+function efGlobalCssJsMessages() {
+		$messages = array(
+		/* English (Ryan Schmidt) */
+				'en' => array(
+						'tog-enableglobalcssjs' => 'Enable Global User CSS and JS',
+				),
+		/* Finnish (Jack Phoenix) */
+				'fi' => array(
+						'tog-enableglobalcssjs' => 'Ota käyttöön käyttäjäkohtainen globaali CSS ja JS',
+				),
+		);
+
+		return $messages;
+}
