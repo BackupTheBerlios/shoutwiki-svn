cba263b7eedba48046fc45512453394f3458752f
ashley <ashley@ba90ee93-4649-0410-b91f-c47b0897d458>
2008-05-24 18:16:53 +0000

    +i18n file for AjaxLogin...I haven't patched AjaxLogin.body.php / AjaxLogin.php to take this into account yet though

---
+++ trunk/mediawiki/extensions/AjaxLogin/AjaxLogin.i18n.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Internationalisation file for the AjaxLogin extension.
+ *
+ * @ingroup Extensions
+ * @author ShoutWiki, Inc.
+ */
+
+$messages = array();
+
+/** English
+ * @author Misza
+ */
+$messages['en'] = array (
+	'ajaxlogin-loggingout' => 'Logging out...',
+	'ajaxlogin-nowloggedout' => 'You are now logged out.<br />Changes will take effect the next time you load a page.',
+);
+
+/** Danish (Dansk)
+ * @author ShoutWiki, Inc.
+ */
+$messages['da'] = array (
+	'ajaxlogin-loggingout' => 'Logge ud...',
+	'ajaxlogin-nowloggedout' => 'Du er nu logget ud.<br />Ændringerne vil træde i kraft næste gang du indlæser en side.',
+);
+
+/** Finnish (Suomi)
+ * @author Jack Phoenix
+ */
+$messages['fi'] = array (
+	'ajaxlogin-loggingout' => 'Kirjaudutaan ulos...',
+	'ajaxlogin-nowloggedout' => 'Olet nyt kirjautunut ulos.<br />Muutokset astuvat voimaan kun seuraavan kerran lataat sivun.',
+);
+
+/** French (Français)
+ * @author ShoutWiki, Inc.
+ */
+$messages['fr'] = array (
+	'ajaxlogin-loggingout' => 'Déconnexion...',
+	'ajaxlogin-nowloggedout' => 'Vous êtes à présent déconnecté(e).<br />Les modifications prendront effet la prochaine fois que vous chargez une page.',
+);
+
+/** Japanese (日本語)
+ * @author ShoutWiki, Inc.
+ */
+$messages['ja'] = array (
+	'ajaxlogin-loggingout' => 'ログアウトして...',
+	'ajaxlogin-nowloggedout' => 'あなたが今すぐログアウト.<br />は、次回の変更はすぐに反映されたページをロードします。',
+);
+
+/** Norwegian (bokmål) (Norsk (bokmål))
+ * @author ShoutWiki, Inc.
+ */
+$messages['no'] = array (
+	'ajaxlogin-loggingout' => 'Logge ut...',
+	'ajaxlogin-nowloggedout' => 'Du er nå logget ut.<br />Endringene trer i kraft neste gang du laste en side.',
+);
