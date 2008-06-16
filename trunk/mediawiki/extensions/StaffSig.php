baba5a607d28153f2ebf5c19a34dd488cc57aeb5
ashley <ashley@ba90ee93-4649-0410-b91f-c47b0897d458>
2008-06-16 22:33:38 +0000

    tweaks for StaffSig extension

--- trunk/mediawiki/extensions/StaffSig.php
+++ trunk/mediawiki/extensions/StaffSig.php
@@ -6,6 +6,13 @@
  * @copyright © 2007-2008 Jack Phoenix
  */
 
+/**
+ * Protect against register_globals vulnerabilities.
+ * This line must be present before any global variable is referenced.
+ */
+if (!defined('MEDIAWIKI')) die();
+
+//Avoid unstubbing $wgParser too early on modern (1.12+) MW versions, as per r35980
 if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
 	$wgHooks['ParserFirstCallInit'][] = 'wfStaffSig';
 } else {
@@ -15,19 +22,18 @@ if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
 $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'StaffSig',
 	'author' => 'Jack Phoenix',
-	'version' => '0.3',
+	'version' => '0.4',
 	'url' => 'http://www.shoutwiki.com',
 	'description' => 'Adds <tt>&lt;staff/&gt;</tt> tag to identify staff members.'
 );
 
 function wfStaffSig() {
-    global $wgParser;
-    $wgParser->setHook('staff', 'renderStaffSig');
+	global $wgParser;
+	$wgParser->setHook('staff', 'renderStaffSig');
 	return true;
 }
 
 function renderStaffSig() {
-    $output  = '<a href="http://www.shoutwiki.com/wiki/ShoutWiki_Staff"><img src="http://staff.shoutwiki.com/i/images/thumb/a/ac/Staffsig.png/60px-Staffsig.png" alt="ShoutWiki Staff"/></a>';
-
-    return $output;
+	$output  = '<a href="http://www.shoutwiki.com/wiki/ShoutWiki_Staff"><img src="http://staff.shoutwiki.com/i/images/thumb/a/ac/Staffsig.png/60px-Staffsig.png" alt="ShoutWiki Staff"/></a>';
+	return $output;
 }
